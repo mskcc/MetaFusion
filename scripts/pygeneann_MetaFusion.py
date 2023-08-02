f556460c976a35238ace885000c48c6c1ab37104
Alexandria Pinto <pintoa1@mskcc.org>
2023-08-02 16:07:16 -0400

    added comments for scoring function, mild clean up.... theres still functions we do not need but I dont want to break anything

--- scripts/pygeneann_MetaFusion.py
+++ scripts/pygeneann_MetaFusion.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 debug=0
 import sys
 import pysam
@@ -884,6 +885,8 @@ class CffFusion():
         # Where highest scoring regions are coding on boundary bps
         # Chance for reann gene to be different from renamed gene
         # Identify region/transcript with highest score  
+        # If there is more than 1 transcript/gene/region with same score, will return the FIRST occurance of the score
+        # This therefore can be somewhat random if there are a lot of transcripts/genes/regions with the same score
         for gname1 in genes1:
             for bpann1 in genes1[gname1]:
                 score1, is_on_boundary1, close_to_boundary1 = self.__cal_score(bpann1, "head")  
@@ -899,13 +902,9 @@ class CffFusion():
                     max_t2 = score2, is_on_boundary2, close_to_boundary2, bpann2
                 elif score2 > max_t2[0]:
                     max_t2 = score2, is_on_boundary2, close_to_boundary2, bpann2
-        #if debug:
-        #    print >> sys.stderr, self.bpann1.gene_name
-        #    print >> sys.stderr, (max_t1[0], max_t2[0] )
-        #    print >> sys.stderr, self.t_gene1
-        #    print >> sys.stderr, self.bpann2.gene_name
-        #    print >> sys.stderr, (max_t1[0], max_t2[0] )
-        #    print >> sys.stderr, self.t_gene2 
+
+        # If max_t1 + max_t2 is greater than previous score (should be zero on initialization, but if checking strand order a/b or d/c will have a previous score)
+        # Assign max_t1 and max_t1 information to fusion
         if max_t1[0] + max_t2[0] > self.score1 + self.score2:
             self.score1, self.gene1_on_bndry, self.gene1_close_to_bndry, self.bpann1 = max_t1
             self.score2, self.gene2_on_bndry, self.gene2_close_to_bndry, self.bpann2 = max_t2
@@ -914,32 +913,31 @@ class CffFusion():
             self.reann_gene2 = self.bpann2.gene_name
             self.reann_type1 = self.bpann1.type
             self.reann_type2 = self.bpann2.type
-
+            ## Checking if both genes are coding genes
             if id1 and id2:
                 if len(set(id1)) != 1 or len(set(id2))!= 1:
                     print >> sys.stderr, "coding id err."
                     print >> sys.stderr, id1, id2
                     sys.exit(1)
+                # How many coding genes are "between" gene1 and gene2
+                # IDX based on the order of the gene_bed you put in. 
+                # First coding gene in gene_bed == idx_1
+                # Second coding gene in gene_bed == idx_2
+                # I believe v75_gene.bed is chr1->chr21->chrX->chrY
                 idx1 = int(id1[0].split("_")[0])
                 idx2 = int(id2[0].split("_")[0])
                 self.coding_id_distance = abs(idx1 - idx2)
             gene_interval1 = gene_ann.get_gene_interval(self.bpann1.gene_name)
             gene_interval2 = gene_ann.get_gene_interval(self.bpann2.gene_name)
 
+            # If genes are on same chromosome, how far apart are they
             if gene_interval1 and gene_interval2:
                 if gene_interval1.chr == gene_interval2.chr:
                     self.gene_interval_distance = max(gene_interval1.start, gene_interval2.start) - min(gene_interval1.end, gene_interval2.end)
 
-            #switch pos order if necessary #SWAP. Really only matters for defuse, other callers don't get gene order wrong
-            #if switch_pos:
-            #    self.chr1, self.chr2 = self.chr2, self.chr1
-            #    self.pos1, self.pos2 = self.pos2, self.pos1
-            #    self.strand1, self.strand2 = self.strand2, self.strand1
-            #    self.t_gene1, self.t_gene2 = self.t_gene2, self.t_gene1
-            #    self.t_area1, self.t_area2 = self.t_area2, self.t_area1
-
             ## assign fusion to a category according to best score gene pair
             # No driver gene
+            # If a gene/loc not in gene bed will occur ,as well as for biological reason 
             if not genes1:
                 category = "NoDriverGene"
             # map to same gene
@@ -947,6 +945,7 @@ class CffFusion():
                 category = "SameGene"
             else:
                 # category fusions into: read through, gene fusion, truncated coding, truncated noncoding, nonsense
+                # If a gene/loc not in gene bed can also get truncated coding/truncated noncoding
                 gene1_is_coding = gene_ann.is_coding(self.reann_gene1)
                 
                 if genes2:
@@ -980,6 +979,7 @@ class CffFusion():
                     print >> sys.stderr, "Warning: Unknown category."
                     print >> sys.stderr, type1, type2
             self.category = category
+            # Return transcript assigned
             self.transcript1 = self.bpann1.transcript_id
             self.transcript2 = self.bpann2.transcript_id
         return ""
